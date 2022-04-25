984e003096a10ef86c8a5d7517cbe3018d1bfd9c
Mateusz Malec <mateusz.malec@microsoft.wsei.edu.pl>
2022-04-25 19:59:39 +0200

    feat: handle submit btn

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
+const form = document.querySelector('form');
 const inputs = document.querySelectorAll('input');
 const movBtn = document.querySelector('#movBtn');
 const selectFrom = document.querySelector('#registersSelectFrom');
 const selectTo = document.querySelector('#registersSelectTo');
 const registerTitles = document.querySelectorAll('.registerListItemTitle');
 
-let selectFromValue = selectTo.selectedOptions[0].value;
-let selectToValue = selectFrom.selectedOptions[0].value;
+let selectedFromValue = selectFrom.selectedOptions[0].value;
+let selectedToValue = selectTo.selectedOptions[0].value;
 
 const $hexRegex = /^[0-9a-fA-F]+$/;
 
 const setActiveClass = tag =>
-  tag.getAttribute('name') === selectFromValue || tag.getAttribute('name') === selectToValue
+  tag.getAttribute('name') === selectedFromValue || tag.getAttribute('name') === selectedToValue
     ? tag.classList.add('activeTitle')
     : tag.classList.remove('activeTitle');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const fromInputValue = Array.from(inputs).find(input => input.name === selectedFromValue).value;
+  const toInput = Array.from(inputs).find(input => input.name === selectedToValue);
+  toInput.value = fromInputValue;
+});
 
 inputs.forEach(input =>
   input.addEventListener('blur', ({ target }) => {
@@ -25,7 +34,7 @@ inputs.forEach(input =>
 
 [selectFrom, selectTo].map((select, index) =>
   select.addEventListener('change', ({ target }) => {
-    index === 0 ? (selectFromValue = target.value) : (selectToValue = target.value);
+    index === 0 ? (selectedFromValue = target.value) : (selectedToValue = target.value);
     registerTitles.forEach(tag => setActiveClass(tag));
   })
 );
